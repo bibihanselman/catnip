99c48ce91ff6b1d04e85f7eadda5103bff788505
bibihanselman <bibihanselman@gmail.com>
2024-07-23 22:44:36 -0400

    Update figg.py

--- figg.py
+++ figg.py
@@ -6,27 +6,14 @@ according to user-defined parameters.
 Original code written by Alex DelFranco
 Adapted by Bibi Hanselman
 Original dated 7 July 2024
-Updated 8 July 2024
+Updated 19 July 2024
 """
 import numpy as np
-#import pandas as pd
-#from pylab import figure, cm
-#import matplotlib
-#from matplotlib.colors import LogNorm
-#import matplotlib.pyplot as plt
 from tqdm import tqdm
 from astropy.visualization import (AsinhStretch, ImageNormalize)
-#import glob
-#import astropy.units as u
-#import math
 from astropy.visualization.interval import ManualInterval
-#from astropy.visualization import ZScaleInterval
-#import astropy.convolution as conv
-#import os
 from astropy.io import fits
-#from astropy.utils.data import get_pkg_data_filename
 from astropy.convolution import Gaussian2DKernel
-#from scipy.signal import convolve as scipy_convolve
 from astropy.convolution import convolve
 import astropy.stats as stats
 import scipy.stats
@@ -169,61 +156,64 @@ def mask_remove(im,imdat):
 
 ######################
 
-def norm(imdat):
+def norm(imdat, custom_im=None):
   """
   Takes a .fits file path and returns a normalized and trimmed fits data file
   with an empty header.
   Inputs: 
-      imdat (dict): dictionary of image processing parameters for the desired image
+      imdat (dict): dictionary of image processing parameters (refer to process()
+        docstring for requirements) for the desired image
   Output: 
       norm_im (np array): normalized fits object with header that has the extreme values cut
   """
-  
-  # Get fits data
-  im = fits.getdata(imdat['Path'])
+  if custom_im is not None:
+    im = custom_im
+  else:
+    # Get the data simply by opening it with astropy.io.fits
+    im = fits.getdata(imdat['Path'])
 
   # Choose a corresponding cube slice
-  if len(im.shape)!=2:
-    if imdat['Cube Slice'] != '': im = im[imdat['Cube Slice']]
+  if 'Cube Slice' in imdat:
+    if len(im.shape)!=2 and imdat['Cube Slice'] is not None: im = im[imdat['Cube Slice']]
 
   # Remove edges and data behind the mask
-  if imdat['Crop'] != '':
-    im = edge_remove(im,imdat)
-  if imdat['Masks'] in ('TRUE', True):
-    im = mask_remove(im,imdat)
+  if 'Crop' in imdat: 
+      if imdat['Crop'] is not None: im = edge_remove(im,imdat)
+  if 'Radius' in imdat:
+      if imdat['Radius'] is not None: im = mask_remove(im,imdat)
 
-  if imdat['Mode Subtract'] in ('TRUE', True):
+  if imdat['Mode Subtract']:
     mode = scipy.stats.mode(im,axis=None,nan_policy='omit')[0]
     im = im - mode
     im[im < 0] = np.nan
-
+  
   # Normalize the image
-  if imdat['Normalization'] in ('TRUE', True):
-    bounds = imdat['N-Range'].split(':')
+  if imdat['Normalization']:
+    bounds = imdat['N-Range'] #.split(':')
     # Scale lower percentile of the image to 0
     im -= np.nanpercentile(im,float(bounds[0]))
     # Normalize the image to the upper percentile
     norm_im = im/np.nanpercentile(im,float(bounds[1]))
 
   # Find the median of the background by sigma clipping
-  if imdat['Clipping'] in ('TRUE', True):
+  if imdat['Clipping']:
     bkg = stats.sigma_clip(norm_im,imdat['σ'])
     norm_im = norm_im - np.ma.median(bkg)
 
   # Check if we want to smooth
-  if imdat['Smoothing'] in ('TRUE', True):
+  if imdat['Smoothing']:
     # We smooth with a Gaussian kernel
     kernel = Gaussian2DKernel(x_stddev=float(imdat['Stdev']))
     norm_im = convolve(norm_im, kernel, nan_treatment='interpolate')
     
   # Set colorbar bounds for scaling 
-  if imdat['Colorbar'] in ('TRUE', True):
-    cb = imdat['CB-Range'].split(':')
+  if imdat['Colorbar']:
+    cb = imdat['CB-Range'] #.split(':')
     cb = [float(cb[0]),float(cb[1])]
   else: cb = [0,1]
 
   # Log scale the image data (Hyperbolic Arcsine)
-  if imdat['Scaling'] in ('TRUE', True):
+  if imdat['Scaling']:
     if imdat['Curve'] == 'asinh':
       normalize = ImageNormalize(norm_im, interval=ManualInterval(cb[0],cb[1]), stretch=AsinhStretch(imdat['Scale Parameter']))
     if imdat['Curve'] == 'asinhlin':
@@ -232,6 +222,8 @@ def norm(imdat):
                                 b=imdat['Scale Parameter 2'],c=imdat['Scale Parameter 3']))    
   else: normalize = 'linear'
   
+  if 'Nan To Num' in imdat:
+    if imdat['Nan To Num']: norm_im = np.nan_to_num(norm_im)
   # Return the final processed image and its scaling
   return norm_im, normalize
 
@@ -245,10 +237,16 @@ def check_keys(imdt, im):
     # First, check the required keys
     imdat = imdt[im]
     keys = [
-        'Path', 'Cube Slice', 'Crop', 'Dimensions', 'Masks', 'Mode Subtract',
+        'Path', 'Cube Slice', 'Dimensions', 'Mode Subtract',
         'Normalization', 'Clipping', 'Smoothing', 'Colorbar', 'Scaling'
         ]
     missingkeys = [key for key in keys if key not in imdat]
+    
+    # Check for the special case of 'Cube Slice'
+    if 'Path' in imdat:
+        im = fits.getdata(imdat['Path'])
+        if (len(im.shape) != 2) and ('Cube Slice' not in imdat):
+            missingkeys.append('Cube Slice')
     
     # Now check for the 'optional' keys
     # Not an elegant way to do this. Will figure out something better later. 7/8/24
@@ -261,7 +259,7 @@ def check_keys(imdt, im):
         }
     for reqkey in optkeys:
         if reqkey in imdat:
-            if (imdat[reqkey] in ('TRUE', True)) and (optkeys[reqkey] not in imdat):
+            if (imdat[reqkey]) and (optkeys[reqkey] not in imdat):
                 missingkeys.append(optkeys[reqkey])
     #The special case of scaling parameters
     if 'Scaling' in imdat:
@@ -285,84 +283,92 @@ def check_keys(imdt, im):
 
 ######################
 
-def process(imdat):
-  """
-  Processes a set of images with normalization, background subtracting, 
-  convolutional smoothing, etc. based on given parameters for each image.
+def process(imdat, name='dict', custom_ims=None):
+    """
+    Processes a set of images with normalization, background subtracting, 
+    convolutional smoothing, etc. based on given parameters for each image.
   
-  Inputs:
+    Inputs:
       imdat (dict): dictionary in which each key must be the name of the disk 
       whose image is to be processed, and the value of each key a 
-      nested dictionary describing the processing settings for each image 
-      that MUST contain the following keys (with possible values). 
-      Keys marked with * are only required if another key is valued
-      'TRUE' or True.
+      nested dictionary describing the processing settings for each image.
+      There is flexibility in the nested dictionaries that may be passed to
+      this function, but they MUST contain the following keys (with possible 
+      values).
+      REQUIRED PARAMETERS:
           'Path' (str): name of the fits file
-          'Crop' (int): radius to be cropped from the edge of the image
           'Dimensions' (int): length of one side of the image in pixels
-          'Masks' (bool; str, 'TRUE' or 'FALSE'): remove pixels within some 
-              radius from the center? 
-          *'Radius' (float): radius from the center below which pixels are to 
-              be removed
           'Mode Subtract' (bool; str, 'TRUE' or 'FALSE'): subtract the mode
               brightness from each pixel value?
           'Normalization' (bool; str, 'TRUE' or 'FALSE'): normalize the 
               pixel brightnesses?
-          *'N-Range' (str, formatted: lower:upper): percentile bounds of 
-              the normalization
           'Clipping' (bool; str, 'TRUE' or 'FALSE'): perform sigma clipping to
               shave extreme pixel values?
-          *'σ' (float): sigma value at which to clip
           'Smoothing' (bool; str, 'TRUE' or 'FALSE'): perform convolutional 
               smoothing on the image?
-          *'Stdev' (float): standard deviation of smoothing kernel
           'Colorbar' (bool; str, 'TRUE' or 'FALSE'): set custom colorbar bounds?
-          *'CB-Range' (str, formatted: lower:upper): lower and upper bounds
-              of colorbar between 0 and 1
           'Scaling' (bool; str, 'TRUE' or 'FALSE'): apply a nonlinear scale 
               to the colorbar?
-          *'Curve' (str): name of scaling curve
-          *'Scale Parameter' (float): scale parameter for asinh scaling
-          *'Scale Parameter 2' and 'Scale Parameter 3' (floats): additional 
-          scale parameters for asinhlin scaling
-          IF THE FITS FILE IS 3D:
-              'Cube Slice' (int): index of desired fits cube slice
+          'Cube Slice' (int): index of desired fits cube slice
+              (stokes cubes only)
+      CONTINGENT PARAMETERS (only needed if associated key is valued True):
+          'Normalization' -> 'N-Range' (str, formatted: lower:upper):
+              percentile bounds of the normalization
+          'Clipping' -> 'σ' (float): sigma value at which to clip
+          'Smoothing' -> 'Stdev' (float): standard deviation of smoothing kernel
+          'Colorbar' -> 'CB-Range' (str, formatted: lower:upper): lower and 
+              upper bounds of colorbar; values must be between 0 and 1
+          'Scaling' ->
+              'Curve' (str): name of scaling curve
+              'Scale Parameter' (float): scale parameter for asinh scaling
+              'Scale Parameter 2' and 'Scale Parameter 3' (floats): additional 
+              scale parameters for asinhlin scaling
+      OPTIONAL PARAMETERS:
+          'Crop' (int): radius (in pixels) to be cropped from the edge of the image
+          'Radius' (int): radius (in pixel) from the center within which
+              pixels are to be removed
+          'Nan To Num' (bool): convert nans to 0?
           
     Outputs:
-        prc_ims (dict): dictionary containing processed images. 
-        The keys are the names of the disks, and the values are dictionaries 
-        containing the corresponding processed image data (key: 'Image') and 
-        scaling curve (key: 'Scaling').          
-  """
-  # Access global variables
-  global old_prc_ims, old_prc_scales, old_imdat
-  
-  # Check if the dictionary has been updated since the last processing
-  if imdat == old_imdat:
-      print('All images already processed!')
-      return old_prc_ims, old_prc_scales
-  else:
-      # Initialize return dicts
-      prc_ims, prc_scales = {}, {}
-      
-      # For each image, normalize and return the image data
-      for im in tqdm(imdat):
-        # First, check if the image has already been processed.
-        # Avoids redundant processing.
-        if (im in old_prc_ims) and (im in old_imdat):
-            if (imdat[im] == old_imdat[im]):
-                # Don't go through processing; just use the old image
-                prc_ims[im], prc_scales[im] = old_prc_ims[im], old_prc_scales[im]
-                continue
-        # Otherwise, process the image
-        check_keys(imdat, im) # Check if the right keys are there before proceeding 
-        prc_ims[im], prc_scales[im] = norm(imdat[im])
+        prc_ims (dict): processed image data        
+        prc_scales (dict): scaling curve for each image
+    """
+    # Access global variables
+    global old_prc_ims, old_prc_scales, old_imdat
+    old_dicts = (old_prc_ims, old_prc_scales, old_imdat)
     
-      #Save the current dictionaries
-      old_prc_ims = prc_ims
-      old_prc_scales = prc_scales
-      old_imdat = imdat
+    # Create a new dict inside the global variables if the key isn't there
+    if all(name not in old_dict for old_dict in old_dicts):
+        for old_dict in old_dicts: old_dict[name] = {}
+    
+    # Check if the dictionary has been updated since the last execution
+    if imdat == old_imdat[name]:
+        print('All images already processed!')
+        return old_prc_ims[name], old_prc_scales[name]
+    else:
+        # Initialize return dicts
+        prc_ims, prc_scales = {}, {}
+
+        # For each image, normalize and return the image data
+        for im in tqdm(imdat, desc=f"Processing {name}"):
+            # First, check if the image has already been processed.
+            # Avoids redundant processing.
+            if all(im in old_dict[name] for old_dict in old_dicts):
+                if (imdat[im] == old_imdat[name][im]):
+                    # Don't go through processing; just use the old image
+                    prc_ims[im], prc_scales[im] = old_prc_ims[name][im], old_prc_scales[name][im]
+                    continue
+            # Otherwise, process the image
+            check_keys(imdat, im) # Check if the right keys are there before proceeding 
+            if custom_ims is None:
+                prc_ims[im], prc_scales[im] = norm(imdat[im])
+            else: prc_ims[im], prc_scales[im] = norm(imdat[im], custom_im=custom_ims[im])
+    
+        #Save the current dictionaries
+        old_prc_ims[name] = prc_ims
+        old_prc_scales[name] = prc_scales
+        old_imdat[name] = imdat
       
-      # Return the dictionary of normalized images. Use prc_scales to set
-      # norm keyword when using plt.imshow().
-      return prc_ims, prc_scales
+        # Return the dictionary of normalized images. Use prc_scales to set
+        # norm keyword when using plt.imshow().
+        return prc_ims, prc_scales
